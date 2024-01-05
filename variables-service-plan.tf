9df55573889e6962b8fd7cf04df837a5ce5be270
audunsolemdal <audun@solom.no>
2024-01-05 13:03:56 +0100

    Add default null value

--- variables-service-plan.tf
+++ variables-service-plan.tf
@@ -1,6 +1,7 @@
 variable "service_plan_id" {
   description = "ID of the existing Service Plan that hosts the App Service. Leave empty to create a new plan."
   type        = string
+  default     = null
 }
 
 locals {
